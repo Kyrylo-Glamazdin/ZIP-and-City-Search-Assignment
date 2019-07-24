86b2244c2491934fe11d38e323a36f6a81e220bd
mehrabha <mehrabhfz@gmail.com>
2019-07-24 11:37:49 -0400

    Nav

---
+++ zip-app/src/Nav.js
@@ -0,0 +1,21 @@
+import React from "react"
+import "./Nav.css"
+
+class Nav extends React.Component {
+	constructor() {
+	  super();
+	  this.state = {
+	  
+	  }
+	}
+
+	render() {
+		return(
+		  <div className="Nav">
+		    Zipcode Search
+		  </div>
+		);
+	}
+}
+
+export default Nav;

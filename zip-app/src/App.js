199adb108b68cad4335ad1d0345cddd98be91831
Mahdiur Rahman <rmahdiur@gmail.com>
2019-07-25 10:45:11 -0400

    close #1

--- zip-app/src/App.js
+++ zip-app/src/App.js
@@ -15,16 +15,44 @@ class App extends Component {
 			]
 		}
 	}
-  render() {
-    return (
-	  <div className="App Container">
-	    <Nav>
-		</Nav>
-		<ZipForm searchZip="10016" />
-		<Info cities={this.state.cities}/>
-	  </div>
-    );
-  }
+
+	getZipInfo = zipcode => {
+		let xhr = new XMLHttpRequest()
+		let url = "http://ctp-zip-api.herokuapp.com/zip/" + zipcode
+		data = []
+	
+		xhr.open("GET", url, true)
+	
+		xhr.onload = function() {
+			if ((this.status == 200) && (this.readyState == 4)) {
+				let xhrJSON = JSON.parse(xhr.response)
+				console.log(xhrJSON)
+				for (let i = 0; i < xhrJSON.length; i++) {
+					data[i] = [xhrJSON[i].City, xhrJSON[i].State, xhrJSON[i].Lat, xhrJSON[i].Long, xhrJSON[i].EstimatedPopulation, xhrJSON[i].TotalWages]
+				}
+			}
+			else {
+				infoP.innerHTML = "No results found for \"" + zipcode + "\"" 
+				console.log("ERROR")
+			}
+		}
+		
+		xhr.send()
+		
+		return data
+	}
+
+
+  	render() {
+		return (
+			<div className="App Container">
+				<Nav>
+				</Nav>
+				<ZipForm searchZip="10016" />
+				<Info cities={this.state.cities}/>
+			</div>
+		);
+  	}
 }
 
 export default App;
